36bdda3ba7e5aff27ad87f51a3ba074e6162c38b
José Ignacio <joseignacioig9910@gmail.com>
2022-10-16 20:04:25 +0200

    obligatorio

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res, next) => {
+    Movies.find()
+    .then((response) => {
+        res.render("movies.hbs", {
+            allMovies: response
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+router.get('/movies/:movieId', (req, res, next) => {
+    Movies.findById(req.params.movieId)
+        .then((response) => {
+            console.log(response)
+            res.render('one-movie', {
+                movieOnly: response
+            })
+        })
+        .catch(err => console.log(err));
+});
 
 module.exports = router;
